ea9b4a416c03553889c233938f30f79f28e851e3
zhangxin <1179252593@qq.com>
2022-08-11 10:56:54 +0800

    添加了修改说明

--- IdeaProjects/FirstStudy/TestException/src/com/msb/test1/ceshi2.java
+++ IdeaProjects/FirstStudy/TestException/src/com/msb/test1/ceshi2.java
@@ -1,5 +1,6 @@
 package com.msb.test1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -7,25 +8,28 @@ public class ceshi2 {
 
     public static void main(String[] args) {
         //实现一个功能：键盘录入两个数，求商：
-        try{
+        try {
             Scanner sc = new Scanner(System.in);
             System.out.println("请录入第一个数：");
             int num1 = sc.nextInt();
             System.out.println("请录入第二个数：");
             int num2 = sc.nextInt();
-            System.out.println("商："+num1/num2);
-        }catch(Exception ex){
+            System.out.println("商：" + num1 / num2);
+        }
+
+        catch(ArithmeticException |InputMismatchException ex){
+            System.out.println("对不起，除数不可以为0");
+        }
+        catch(Exception ex){
             System.out.println("对不起，程序出现异常！");
         }
-        System.out.println("----谢谢你使用计算器111");
-        System.out.println("----谢谢你使用计算器222");
-        System.out.println("----谢谢你使用计算器333");
-        System.out.println("----谢谢你使用计算器444");
-        System.out.println("----谢谢你使用计算器555");
-        System.out.println("----谢谢你使用计算器666");
-        System.out.println("----谢谢你使用计算器777");
-        System.out.println("----谢谢你使用计算器888");
-        System.out.println("----谢谢你使用计算器zuihou1yici");
+        finally
+         {
+            System.out.println("我就是试一试最后");
+        }
+        System.out.println("----谢谢你使用计算器");
+
+
     }
 
 }
---
+++ IdeaProjects/FirstStudy/TestException/src/com/msb/test1/ceshi3.java
@@ -0,0 +1,23 @@
+package com.msb.test1;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+
+
+public class ceshi3 {
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        //检查异常：
+
+       /* try {
+            Class.forName("com.msb.test01.Test").newInstance();//option+enter快捷键选择try-catch进行包围
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            //throw new RuntimeException(e);
+            e.printStackTrace();
+        }*/
+        Class.forName("com.msb.test01.Test").newInstance();
+
+    }
+}
+
