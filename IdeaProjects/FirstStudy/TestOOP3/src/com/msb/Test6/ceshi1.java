e399c55b3d6a47b8fcf16b27ec83d4a9bdb465fa
Tcygg <1179252593@qq.com>
2022-12-28 00:10:50 +0800

    nnn

--- IdeaProjects/FirstStudy/TestOOP3/src/com/msb/Test6/ceshi1.java
+++ IdeaProjects/FirstStudy/TestOOP3/src/com/msb/Test6/ceshi1.java
@@ -10,6 +10,7 @@ public class ceshi1 {
     public static void main(String[] args) {
         System.out.println(111);
         System.out.println(222);
+        System.out.println(333);
     }
 
 }
