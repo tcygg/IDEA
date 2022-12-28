01799503fd3960d6cdd04d0f76917e93588297aa
zhangxin <1179252593@qq.com>
2022-12-28 10:11:11 +0800

    1

--- IdeaProjects/FirstStudy/TestOOP3/src/com/msb/Test6/ceshi1.java
+++ IdeaProjects/FirstStudy/TestOOP3/src/com/msb/Test6/ceshi1.java
@@ -11,6 +11,7 @@ public class ceshi1 {
         System.out.println(111);
         System.out.println(222);
         System.out.println(333);
+        System.out.println("wojiushishiysi");
     }
 
 }
