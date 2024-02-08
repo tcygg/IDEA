171db087eb7d41eea33caaed0569d04a6d74b1a1
zhangxin <1179252593@qq.com>
2024-02-08 19:34:19 +0800

    ceshi1zuix1

---
+++ Javaweb_Code/src/main/java/com/example/javaweb_code/ceshi1.java
@@ -0,0 +1,15 @@
+package com.example.javaweb_code;
+
+import java.sql.SQLOutput;
+
+/**
+ * @Auther: TcYgg
+ * @Date: 2024/2/7 - 02 - 07 - 19:36
+ * @Description: com.example.javaweb_code
+ * @version: 1.0
+ */
+public class ceshi1 {
+    public static void main(String[] args) {
+        System.out.println("fdjgjfhsg");
+    }
+}
---
+++ ServletProject/ServletDome1/src/main/java/com/ygg/servletdome1/MyServlet.java
@@ -0,0 +1,40 @@
+package com.ygg.servletdome1;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+/**
+ * @Auther: TcYgg
+ * @Date: 2024/2/8 - 02 - 08 - 17:12
+ * @Description: com.example.servletdemo1
+ * @version: 1.0
+ */
+public class MyServlet extends HttpServlet {
+    /*
+     * 可以接收浏览器的请求
+     * 并作出运算和响应
+     * service Servlet服务方法
+     *
+     * */
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 动态生成数据
+        int num =new Random().nextInt();
+        String message = num%2==0?"happy birthday":"happy new year";
+        // 对浏览器作出响应
+        PrintWriter writer = response.getWriter();// 该打印流指向了浏览器
+//        writer.write(message);
+//        writer.write(num+"东山街道更换时间段");
+
+        String message1 = "<h1>"+message+"</h1>";
+        writer.write(message1);
+
+
+
+
+    }
+}
